adae8737a4263f1c88b7a11eb73dbcb5bbb4a91f
Allan Im <allan@vogle.com>
2019-08-05 15:34:02 -0400

    update design

--- AutosApp/extensions/RoundedCornerView.swift
+++ AutosApp/extensions/RoundedCornerView.swift
@@ -15,7 +15,6 @@ class RoundedCornerView: UIView {
     @IBInspectable var cornerRadius: CGFloat = 0 {
         didSet {
             layer.cornerRadius = cornerRadius
-            layer.masksToBounds = cornerRadius > 0
         }
     }
     
